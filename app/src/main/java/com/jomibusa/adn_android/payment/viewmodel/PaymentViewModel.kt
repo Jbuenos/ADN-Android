f4eafff5963f37f438d3321b744745205350eda0
José Bueno <jomibusadeveloper@gmail.com>
2022-07-03 17:10:36 -0500

    Se sube interfaz gráfica para la capa de la presentación

---
+++ app/src/main/java/com/jomibusa/adn_android/payment/viewmodel/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package com.jomibusa.adn_android.payment.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/jomibusa/adn_android/register/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.jomibusa.adn_android.register.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
