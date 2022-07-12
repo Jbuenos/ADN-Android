c8e8b91b3e15f462ced4f777bf5ee2adc5009fa7
José Bueno <jomibusadeveloper@gmail.com>
2022-07-11 23:29:00 -0500

    Se inicia implementación de hilt para interfaz que es implementada multiples veces

--- app/src/main/java/com/jomibusa/adn_android/register/di/RegisterModule.kt
+++ app/src/main/java/com/jomibusa/adn_android/register/di/RegisterModule.kt
@@ -2,19 +2,31 @@ package com.jomibusa.adn_android.register.di
 
 import com.jomibusa.domain.register.repository.RegisterRepository
 import com.jomibusa.infrastructure.register.repository.RoomCarParkingRegisterRepository
+import com.jomibusa.infrastructure.register.repository.RoomMotorcycleParkingRegisterRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import javax.inject.Qualifier
 
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class RegisterModule {
 
+    /*@Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class RegisterCarRepository
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class RegisterMotorcycleRepository*/
+
     @Binds
+    //@RegisterCarRepository
     abstract fun bindRegisterCarRepository(roomRepository: RoomCarParkingRegisterRepository): RegisterRepository
 
     /*@Binds
+    @RegisterMotorcycleRepository
     abstract fun bindRegisterMotorcycleRepository(roomRepository: RoomMotorcycleParkingRegisterRepository): RegisterRepository*/
 
 }
